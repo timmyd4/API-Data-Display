18f417d97fcaf196320c37a7827ca4f7c5397d70
Tim <timmywilliams4665@gmail.com>
2025-10-28 18:31:21 -0700

    test run for trial data

--- Components/Item.jsx
+++ Components/Item.jsx
@@ -1,6 +1,35 @@
+import {useSate, useEffect} from "react";
+
 export default function Item()
 {
+
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("https://api.sampleapis.com/coffee/hot");
+                if(!res.ok)
+                {
+                    throw new Error("Network Error")
+                }
+                const jsonData = await res.json();
+                setData(jsonData);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return(
-        <p> wOrDs </p>
+        <div className="container">
+            <p> wOrDs </p>
+        </div>
     )
 }
